d286b9d2f6bf2a0a20a0690eca290dfd0c4dbbbe
Nathan Heath <nathanheath92@gmail.com>
2025-04-23 06:00:48 -0400

    Delete osquery_monitor.py

--- osquery_monitor.py
+++
@@ -1,7 +0,0 @@
-import os
-import json
-def query_osquery(query):
-    result = os.popen(f"osqueryi --json 'SELECT * FROM logged_in_users;'").
-    parsed = json.loads(result)
-    for row in parsed:
-        print(f"User: {row['username']}, Login Time: {row['time']}")
